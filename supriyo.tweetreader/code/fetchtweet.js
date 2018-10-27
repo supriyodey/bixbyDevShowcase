7765ef00fd8854f375aaf5b094298809185681e6
supriyodey <supriyo3.14@gmail.com>
2018-10-27 19:19:19 +0530

    Update fetchtweet.js

--- supriyo.tweetreader/code/fetchtweet.js
+++ supriyo.tweetreader/code/fetchtweet.js
@@ -1,10 +1,10 @@
-var url = "https://spreadsheets.google.com/feeds/list/1--daGdaudNwIuQFyXEcLsWRPTegF-aqTkEeahWaLhps/od6/public/basic?alt=json"
+var url = " "    //here goes the google sheet url
 
 
 module.exports.function = function gettweet () {
   
   var result = http.getUrl(url , {format:"json"})
-  var tweet = result.feed.entry[0].title.$t;
+  var tweet = result.feed.entry[0].title.$t;      //fetching the first entry of the sheet
   
   return [{"Tweet": tweet}]
 }
